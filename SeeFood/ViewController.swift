6751734e426a3a22b598c443b8b695e50d447051
copyong <copyong@copyongs-Mac.local>
2019-03-25 22:32:13 +0800

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by copyong on 25/3/19.
+//  Copyright © 2019 copyong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
